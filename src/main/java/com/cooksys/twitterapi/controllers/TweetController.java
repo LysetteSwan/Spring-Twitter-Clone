ba02146e5dce1a3809cf8ea8796a4a38c3f389c8
Lysette Swan <lysetteg1023@gmail.com>
2022-11-02 15:30:47 -0700

    added boilerplate code

--- src/main/java/com/cooksys/twitterapi/controllers/TweetController.java
+++ src/main/java/com/cooksys/twitterapi/controllers/TweetController.java
@@ -1,8 +1,20 @@
 package com.cooksys.twitterapi.controllers;
 
 
+import com.cooksys.twitterapi.dtos.ContextDto;
+import com.cooksys.twitterapi.dtos.TweetResponseDto;
+import com.cooksys.twitterapi.dtos.UserResponseDto;
 import com.cooksys.twitterapi.services.TweetService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties.Credential;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +23,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tweet")
 public class TweetController {
 
-//    private final TweetService tweetService;
+   private final TweetService tweetService;
+   
+   @GetMapping 
+   public List<TweetResponseDto> getAllTweets() {
+     return tweetService.getAllTweets();
+   }
+   
+   @GetMapping("/{id}/mentions")
+   public List<UserResponseDto>getMentionsById(@PathVariable Long id){
+	   return tweetService.getMentionsById(id);
+   }
+   
+   
+   @GetMapping("/{id}/context")
+   public ContextDto getContextById(@PathVariable Long id){
+	   return tweetService.getContextById(id);
+   }
+   
+   @PostMapping("/{id}/repost")
+   //@ResponseStatus(HttpStatus.CREATED)
+   public TweetResponseDto repostTweetById(@PathVariable Long id, @RequestBody Credential credential ) {
+	   return tweetService.repostTweetById(id, credential);
+   }
+   
+   @DeleteMapping("/{id}")
+   public TweetResponseDto deleteTweetById(@PathVariable Long id, @RequestBody Credential credential) {
+	   return tweetService.deleteTweetbyId(id, credential);
+   }
+   
 }
--- src/main/java/com/cooksys/twitterapi/services/TweetService.java
+++ src/main/java/com/cooksys/twitterapi/services/TweetService.java
@@ -1,4 +1,22 @@
 package com.cooksys.twitterapi.services;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties.Credential;
+
+import com.cooksys.twitterapi.dtos.ContextDto;
+import com.cooksys.twitterapi.dtos.TweetResponseDto;
+import com.cooksys.twitterapi.dtos.UserResponseDto;
+
 public interface TweetService {
+
+	List<TweetResponseDto> getAllTweets();
+
+	List<UserResponseDto> getMentionsById(Long id);
+
+	ContextDto getContextById(Long id);
+
+	TweetResponseDto repostTweetById(Long id, Credential credential);
+
+	TweetResponseDto deleteTweetbyId(Long id, Credential credential);
 }
